4f0bded32834a56fded5411331296f33464688ab
student-14 <terleckawika123@gmail.com>
2022-10-18 14:07:31 +0200

    Dodanie kolorów i layoutu 2 aktywności

--- app/src/main/java/com/taboret/zdjecie1/MainActivity2.java
+++ app/src/main/java/com/taboret/zdjecie1/MainActivity2.java
@@ -3,6 +3,7 @@ package com.taboret.zdjecie1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        SeekBar seekbar1 = (SeekBar) findViewById(R.id.seekbar1);
+        int seekBarValue= seekbar1.getProgress();
+
     }
 }
